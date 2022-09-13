f724a9f36ed78283a292b918b9149d8750075164
ChristianNomena <christiangrzk@gmail.com>
2022-09-13 08:49:05 +0300

    fix project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='HomePage'>
+            <div className='container flex justify-center items-center mx-auto'>
+                <h1 className='text-primary'>Dieu nous aime tous</h1>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
