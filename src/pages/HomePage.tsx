38f45bd774d962e12a16d30db5e6c19615e0a3d1
ChristianNomena <christiangrzk@gmail.com>
2022-09-14 21:32:25 +0300

    add navbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const HomePage = () => {
     return (
         <div className='HomePage'>
+            <Navbar />
+
             <div className='site__blocks--home'>
                 <div className='container full-page flex justify-center items-center'>
-                    <h1 className='text-light uppercase text-5xl font-semibold'>Dieu nous aime tous</h1>
+                    <h1 className='text-light uppercase text-4xl font-semibold'>Dieu nous aime tous</h1>
                 </div>
+            </div>
+
+            <div className='container py-64'>
+                <p>Hey there !!!</p>
             </div>
         </div>
     );
